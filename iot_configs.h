993befa6bda28208d68041d940ef80bc6c833e6e
MatthewEdelman <edelmanm9@students.rowan.edu>
2021-12-06 15:02:18 -0500

    ESP Device Code Completed
    The ESP still needs a hardware restart to get proper readings out of the digital pins

---
+++ iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "60staghorn"
+#define IOT_CONFIG_WIFI_PASSWORD "We hold these truths to be self evident"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "ESPIoTDemoHub.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "GardenSensorTest1"
+#define IOT_CONFIG_DEVICE_KEY "qqI0nfGnjWvHeRTjxUR3NmagDSK1EKralc7zQd/pXow="
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 15000
